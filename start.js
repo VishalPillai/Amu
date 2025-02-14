33c6f0821704002d1b9d83dcc8642a17cd51df3f
Vishal Pillai <68294182+VishalPillai@users.noreply.github.com>
2025-02-14 12:22:00 +0530

    Update start.js

--- start.js
+++ start.js
@@ -2,6 +2,6 @@ function pageRedirect(){
     var delay = 6000;
 
     setTimeout(function(){
-        window.location = "index.html";},delay);
+        window.location = "index1.html";},delay);
     
 }
